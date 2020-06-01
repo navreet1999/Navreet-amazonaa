405c68a2156c307b9179213f91d86030831bcce6
NAVREET DHIMAN <navreetkaur908@gmail.com>
2020-06-01 08:27:53 +0530

    added

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,6 @@
 export default {
-  MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://dbUser:todoDB@cluster0-ku63s.mongodb.net/user?retryWrites=true&w=majority',
+  MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://dbuser:amazonaa@cluster0-85mws.mongodb.net/test?retryWrites=true&w=majority',
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb'
+  
 }
